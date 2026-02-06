4675b984ffa226c559d8739ba96f92eadb50dea6
kalumurimeghana-creator <kalumurimeghana@gmail.com>
2026-02-06 11:57:42 +0530

    23-01-2026

---
+++ DI-ECommerceApp-Autowired/src/main/java/com/klu/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.klu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.klu.model")
+public class AppConfig {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
